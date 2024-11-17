d474c2fc921eb7a9fa7dc886faf1d3ecaf6f98db
cerro88 <bcerro245@gmail.com>
2024-11-17 21:37:46 +0100

    calculadora felina js

--- MASTERDOTS/js/app.js
+++ MASTERDOTS/js/app.js
@@ -1,3 +1,22 @@
-/CALCULO DE AÑOS FELINOS/
-var edadgato=1;
-console.log(edadgato);
+//CALCULO DE AÑOS FELINOS
+
+//variable
+var edadhumana=0;
+
+console.log("GATO   HUMANO")
+console.log("-------------")
+
+//bucle
+for(let edadgato= 1; edadgato < 22; edadgato++) {
+    if(edadgato==1){
+        edadhumana=15
+    }else if(edadgato==2){
+        edadhumana=24;
+    }else{
+        edadhumana=edadhumana+4;
+    }
+    console.log("   "+edadgato+"    "+edadhumana);
+}
+
+
+
