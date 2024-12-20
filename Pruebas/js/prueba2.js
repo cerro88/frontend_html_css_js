9353e9926bbe846e715b30adbfd8ffc227fd868c
cerro88 <bcerro245@gmail.com>
2024-12-20 19:57:12 +0100

    prueba2js

--- Pruebas/js/prueba2.js
+++ Pruebas/js/prueba2.js
@@ -9,6 +9,34 @@
 // crear variables que almacenen cada uno de los objetos de los formularios: nombre, 
 // email, edad, videojuego y plataforma.
 
-//Capturar el name 
+// Capturar el elemento con ID 'name'
+const nameInput = document.getElementById('name');
+// Imprimir el tipo de nodo 
+console.log(nameInput.nodeType);
+// Asignar el valor "Belén" al input
+nameInput.value = "Belén";
+// Imprimir el valor del input
+console.log(nameInput.value);
+// Imprimir todo el elemento input en la consola
+console.log(nameInput);
 
+
+//Capturar el elemento con ID 'email'
+const emailInput = document.getElementById('email');
+// Imprimir el tipo de nodo 
+console.log(emailInput.nodeType);
+// Asignar un valor al input
+emailInput.value = "bcerro@prueba.net";
+// Imprimir el valor del input
+console.log(emailInput.value);
+// Imprimir todo el elemento input en la consola
+console.log(emailInput);
+
+//Capturar el elemento con ID 'edad'
+const edadInput = document.getElementById('edad');
+// Imprimir el tipo de nodo 
+console.log(edadInput.nodeType);
+// Imprimir todo el elemento input en la consola
+console.log(edadInput);
+console.log(nameInput.options[nameInput.selectedIndex].text)
 
