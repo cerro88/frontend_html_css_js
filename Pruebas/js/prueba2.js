6a8cfbfbf0191a5b76537817de18268180e488a1
cerro88 <bcerro245@gmail.com>
2024-12-20 09:28:02 +0100

    ejercicio

---
+++ Pruebas/js/prueba2.js
@@ -0,0 +1,14 @@
+//PRUEBA
+
+//Queremos poner a prueba lo que has aprendido y para ello seguiremos con nuestro 
+// formulario que recoge datos de los jugadores de videojuegos.
+
+//En este caso vamos a preparar nuestro JS para interactuar con cada 
+// uno de los componentes. 
+// Como vamos a comprobar todos los campos necesitaremos
+// crear variables que almacenen cada uno de los objetos de los formularios: nombre, 
+// email, edad, videojuego y plataforma.
+
+//Capturar el name 
+
+
