3376d59f3218c07d432b0eb610e269ac48acb576
cerro88 <bcerro245@gmail.com>
2025-03-13 23:45:47 +0100

    calculos ok, introducir comprobaciones y mensaje de error

--- MASTERDOTS/Conversor_moneda/js/app.js
+++ MASTERDOTS/Conversor_moneda/js/app.js
@@ -1,24 +1,45 @@
 
-
 /*variables de valor moneda en euros */
 const tasaDolar= 0.92;
 const tasaLibra= 1.19;
 const tasaYen= 0.0062;
 
-/*variables de los resultados */
-let resultadoDolar = euros * tasaDolar;
-let resultadoLibra = euros * tasaLibra;
-let resultadoYen = euros * tasaYen;
-
-/*capturar datos del user */
-document.getElementById("moneda").value
 
 
-/* */
-document.getElementById("dolar").value = resultadoDolar;
-document.getElementById("libra").value = resultadoLibra;
-document.getElementById("yenes").value = resultadoYen;
 
-/*pasar de string a number */
-/*usar parse int no incluye decimales */
-let cantidad = parseFloat(document.getElementById("euros").value);
+//Busca el botón convertir y agrega un evento al botón para cuando el usuario haga click
+document.getElementById("convertir").addEventListener("click", function() {
+    
+    //pasar de string a number 
+    //usar parse int no incluye decimales por eso se usa parseFloat
+
+    //capturar datos introducidos del user 
+    let cantidad = parseFloat(document.getElementById("euros").value);
+
+    //validar que el usuario inserta un número
+    //si cantidad ! ES un número ejecuta el código dentro de if 
+    //(NaN Not a Number, sin ! la negación se estaría comprobando si no es un número)
+    if(!isNaN(cantidad)) {
+
+        /*variables de los resultados */
+        let resultadoDolar = cantidad * tasaDolar;
+        let resultadoLibra = cantidad * tasaLibra;
+        let resultadoYen = cantidad * tasaYen;
+
+        // Mostrar los resultados en los inputs correspondientes
+        document.getElementById("dolar").value = resultadoDolar.toFixed(2);
+        document.getElementById("libra").value = resultadoLibra.toFixed(2);
+        document.getElementById("yenes").value = resultadoYen.toFixed(2);
+
+    } else {
+        alert("Por favor , introduce una cantidad valida en euros.")
+    }
+
+
+});
+
+
+
+
+
+
