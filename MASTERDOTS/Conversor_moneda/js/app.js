67930e0d25a0758031100e795a1b4f90b76cefbd
cerro88 <bcerro245@gmail.com>
2025-03-14 07:07:25 +0100

    falta cambiar algo, porque si introduzco un simbolo después del número sigue haciendo la conversión....

--- MASTERDOTS/Conversor_moneda/js/app.js
+++ MASTERDOTS/Conversor_moneda/js/app.js
@@ -1,44 +1,66 @@
 
-/*variables de valor moneda en euros */
+//Valores en €
 const tasaDolar= 0.92;
 const tasaLibra= 1.19;
 const tasaYen= 0.0062;
 
 
+//Inicialización de variables y obtención de elementos del DOM
+const eurosInput = document.getElementById("euros");
 
+//Captura el elemento donde se mostrarán los mensajes de error
+const error = document.getElementById("error");
 
-//Busca el botón convertir y agrega un evento al botón para cuando el usuario haga click
+//Busca el botón convertir y agrega un evento al botón para cuando el usuario haga clic
 document.getElementById("convertir").addEventListener("click", function() {
     
     //pasar de string a number 
-    //usar parse int no incluye decimales por eso se usa parseFloat
+    //usar parseInt no incluye decimales por eso se usa parseFloat 
+    let cantidad = parseFloat(eurosInput.value);
 
-    //capturar datos introducidos del user 
-    let cantidad = parseFloat(document.getElementById("euros").value);
+    //validar que el campo no esta vacio
+    if (eurosInput.value.trim() === "") {
+        console.log("No se ha especificado cantidad");
+        eurosInput.focus();
+        error.innerText = "Debe expecificar una cantidad en euros";
 
+    }
+    
     //validar que el usuario inserta un número
-    //si cantidad ! ES un número ejecuta el código dentro de if 
-    //(NaN Not a Number, sin ! la negación se estaría comprobando si no es un número)
-    if(!isNaN(cantidad)) {
+    //is not a number
+    else if (isNaN(cantidad)){
+        console.log("La cantidad ingresada no es un número válido");
+        eurosInput.focus();
+        error.innerText = "Debe ingresar un número válido";
+        
+    }
+    
+    //validar que no es menor a 0
+    else if (cantidad < 0 ){
+        console.log("No se pueden ingresar cantidades negativas");
+        eurosInput.focus();
+        error.innerText = "La cantidad debe ser mayor a 0";
 
-        /*variables de los resultados */
-        let resultadoDolar = cantidad * tasaDolar;
-        let resultadoLibra = cantidad * tasaLibra;
-        let resultadoYen = cantidad * tasaYen;
+    }
+    
+    //si todo se cumple se hace conversión
+    else {
+        // Limpiar el mensaje de error
+         error.innerText = "";  
 
-        // Mostrar los resultados en los inputs correspondientes
-        document.getElementById("dolar").value = resultadoDolar.toFixed(2);
-        document.getElementById("libra").value = resultadoLibra.toFixed(2);
-        document.getElementById("yenes").value = resultadoYen.toFixed(2);
+         //conversiones 
+         let resultadoDolar = cantidad * tasaDolar;
+         let resultadoLibra = cantidad * tasaLibra;
+         let resultadoYen = cantidad * tasaYen;  
 
-    } else {
-        alert("Por favor , introduce una cantidad valida en euros.")
+         // Mostrar los resultados en los inputs correspondientes con 2 decimales
+         document.getElementById("dolar").value = resultadoDolar.toFixed(2);
+         document.getElementById("libra").value = resultadoLibra.toFixed(2);
+         document.getElementById("yenes").value = resultadoYen.toFixed(2);
     }
 
 
 });
-
-
 
 
 
